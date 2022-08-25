777f7987acf0d0fc881820129743e5f747bc0921
Rafaela <rafamz05@hotmail.com>
2022-08-25 09:34:48 -0300

    Coloquei os nomes no gráfico

--- geragrafico.py
+++ geragrafico.py
@@ -1,8 +1,14 @@
 from leitorarquivo import LeitorArquivo
+import numpy as np
+import matplotlib.pyplot as plt
  
 def main():
-    leitor = LeitorArquivo('data.txt')
+    leitor = LeitorArquivo('data.text')
     valores = leitor.getValores()
     print(valores)
+    plt.plot(valores)
+    plt.ylabel('Valores de entrada')
+    plt.xlabel('Amostragem')
+    plt.show()
  
 main()
