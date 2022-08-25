88193e424b63679cc2f5f9190e25ea638713ec00
Rafaela <rafamz05@hotmail.com>
2022-08-25 09:51:24 -0300

    Plotando múltiplas

--- geragrafico.py
+++ geragrafico.py
@@ -5,10 +5,12 @@ import matplotlib.pyplot as plt
 def main():
     leitor = LeitorArquivo('data.text')
     valores = leitor.getValores()
+    plt.title('Gráfico de linhas')
     print(valores)
-    plt.plot(valores)
     plt.ylabel('Valores de entrada')
     plt.xlabel('Amostragem')
+    for serie in valores:
+       plt.plot(serie)
     plt.show()
  
 main()
